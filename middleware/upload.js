c6d1cf5cfb89651e31ca4c4c3ae4e75c19d296c9
Vishnuvardhan Reddy Chilakala <110713370+Vishnu539@users.noreply.github.com>
2026-02-10 17:46:28 +0530

    Change upload destination and filename format

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,10 +3,11 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../uploads"));
+    cb(null, "uploads/products");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + "-" + Math.round(Math.random() * 1e9) + ext);
   },
 });
 
