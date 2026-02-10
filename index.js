960310c8d48fc92c5082139ca975c399dd0f6f42
Vishnuvardhan Reddy Chilakala <110713370+Vishnu539@users.noreply.github.com>
2026-02-10 12:41:33 +0530

    Reorganize code structure and logging

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-console.log("userRoutes:", typeof userRoutes);
-console.log("cartRoutes:", typeof cartRoutes);
-console.log("restaurantRoutes:", typeof restaurantRoutes);
-console.log("productRoutes:", typeof productRoutes);
-console.log("orderRoutes:", typeof orderRoutes);
-console.log("adminRoutes:", typeof adminRoutes);
-console.log("restaurantDashboardRoutes:", typeof restaurantDashboardRoutes);
-console.log("restaurantAuthRoutes:", typeof restaurantAuthRoutes);
-
 app.use(
   cors({
     origin: "*",
@@ -33,6 +24,15 @@ const orderRoutes = require("./routes/orderRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const restaurantDashboardRoutes = require("./routes/restaurantDashboardRoutes");
 const restaurantAuthRoutes = require("./routes/restaurantAuthRoutes");
+
+console.log("userRoutes:", typeof userRoutes);
+console.log("cartRoutes:", typeof cartRoutes);
+console.log("restaurantRoutes:", typeof restaurantRoutes);
+console.log("productRoutes:", typeof productRoutes);
+console.log("orderRoutes:", typeof orderRoutes);
+console.log("adminRoutes:", typeof adminRoutes);
+console.log("restaurantDashboardRoutes:", typeof restaurantDashboardRoutes);
+console.log("restaurantAuthRoutes:", typeof restaurantAuthRoutes);
 
 app.use("/api/restaurant", restaurantAuthRoutes);
 app.use("/api/users", userRoutes);
@@ -60,5 +60,6 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 
 });
+
 
 
