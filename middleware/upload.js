d136fb0e20be0a144d567d7a4dba42801041dbc4
Vishnuvardhan Reddy Chilakala <110713370+Vishnu539@users.noreply.github.com>
2026-02-10 17:01:13 +0530

    Refactor upload middleware for improved file naming

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,13 +3,11 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/products");
+    cb(null, path.join(__dirname, "../uploads"));
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
-const upload = multer({ storage });
-
-module.exports = upload;
+module.exports = multer({ storage });
