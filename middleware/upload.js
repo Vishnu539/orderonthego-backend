062e7940715cc97efc27a0d0e806caf226e9ca53
Vishnuvardhan Reddy Chilakala <110713370+Vishnu539@users.noreply.github.com>
2026-02-10 22:04:16 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,3 +1,4 @@
+// server/middleware/upload.js
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
@@ -5,11 +6,11 @@ const cloudinary = require("../config/cloudinary");
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "orderonthego/products",
-    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    folder: "orderonthego",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
   },
 });
 
 const upload = multer({ storage });
 
-module.exports = upload;
+module.exports = { upload };
